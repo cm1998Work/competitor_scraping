ce8e12c46ed066bfe95f7f0a4fad509e65f5d8c4
cm1998Work <146945011+cm1998Work@users.noreply.github.com>
2023-11-30 16:30:04 +0000

    Update ey.py

--- ey.py
+++ ey.py
@@ -1,9 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-def getManagedServicesPageHtml() -> str:
+MS_URL = r'https://www.ey.com/en_uk/managed-services'
 
-  resp = requests.get()
+def scrapeEyManagedServicesWebPage():
+
+  html = getHtml(MS_URL)
+  bs = convertToBeautifulSoup(html)
+
+  thought_articles = getThoughtArticles(bs)
+
+def getHtml(url: str) -> str:
+  r = requests.get(url)
+  html = r.text
+  return html
+
+def convertToBeautifulSoup(html: str) -> BeautifulSoup:
+  return BeautifulSoup(html)
+
+
+def getThoughtArticles(bs: BeautifulSoup) -> list:
+  
+  
 
   
   
